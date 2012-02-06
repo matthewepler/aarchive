cad1eb06433ce2eebbea61fcd8fa7e7613b1e35b
Matthew Epler <matthewepler@gmail.com>
2012-02-06 17:45:03 -0500

    added parser

--- app.rb
+++ app.rb
@@ -459,3 +459,40 @@ get '/contact' do
 </div>
 HTML
 end
+
+
+get 'parser' do
+  username = "118319050543607777197"
+
+albums = Picasa.albums :google_user => username # goes over the internet
+
+albums.each do |album|
+  puts
+  puts "found album: #{album[:id]}"
+  puts "=========================="
+  puts
+
+  a = Can.new
+  
+  album_string = album[:title]
+  a.canType, a.canNum = album_string.split(" #") # ["Samll 16mm", "2"]
+  
+  a.albumURL = album[:slideshow].split("#")[0]
+  a.save
+  #puts a.inspect
+
+  result = Picasa.photos(:google_user => username, :album_id => album[:id])  # goes over the internet
+  
+    # assume the first photo is the can label
+    photo = result[:photos][0]
+        
+    parts = photo[:thumbnail_1].split("/")
+    parts[parts.length - 2] = "s1600"
+    url = parts.join("/")
+    a.imageURL = url
+    a.save
+    puts "photo url: #{url}"
+    
+"Done!"
+end
+end
