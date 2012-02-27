ac6e3d06a425cc852fd539c0041d0d9b3cdf2152
Matthew Epler <matthewepler@gmail.com>
2012-02-27 13:37:52 -0500

    add previous link

--- app.rb
+++ app.rb
@@ -241,7 +241,11 @@ get '/display_record/:id' do
   output +='<a href="/start/1">&#62Database</a>'
   output +='<a href="/about">&#62About</a>'
   output +='<a href="/contact">&#62Contact</a>'
-  output +='<a style="text-align:right;position:relative;left:750" href="/display_record/'
+  output +='<a style="position:relative;left:600" href="/display_record/'
+  prevpage = (params[:id].to_i -1)
+  output += "#{prevpage}"
+  output += '">&#60Prev</a>'
+  output +='<a style="text-align:right;position:relative;left:150" href="/display_record/'
   nextpage =  (params[:id].to_i + 1)
   output += "#{nextpage}"
   output += '">&#62Next</a>'
