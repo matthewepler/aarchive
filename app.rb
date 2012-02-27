cac5e63428de1824d66c4c0a8111d562f14d3750
Matthew Epler <matthewepler@gmail.com>
2012-02-27 13:29:52 -0500

    string in href

--- app.rb
+++ app.rb
@@ -241,10 +241,10 @@ get '/display_record/:id' do
   output +='<a href="/start/1">&#62Database</a>'
   output +='<a href="/about">&#62About</a>'
   output +='<a href="/contact">&#62Contact</a>'
-  output +='<a style="text-align:right;position:relative;left:750" href="/display_record/'
-  nextpage =  (params[:page].to_i + 1)
-  output += '#{nextpage}'
-  output += '">&#62Next</a>'
+  output +='<a style="text-align:right;position:relative;left:750" href="/display_record/#{(params[:page].to_i + 1)}">&#62Next</a>
+  # nextpage =  (params[:page].to_i + 1)
+  # output += '#{nextpage}'
+  # output += '">&#62Next</a>'
   output +='</div>'
   output +="<div class='can-header'>"
   output +="<div class='green-tab'>Canister #{thisCan.canNum} Record</div>"
