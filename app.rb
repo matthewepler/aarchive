5cc42c526123c522ea54db8fd44266a6bf9507c1
Matthew Epler <matthewepler@gmail.com>
2012-03-12 16:39:04 -0400

    added exclamation to elsif

--- app.rb
+++ app.rb
@@ -580,7 +580,7 @@ get '/displaysearch' do
       output +="<img src='#{can.imageURL}' width='100' height='75 /></a>"
       output +="<p><a href='display_record/#{can.id}'>&#62 Can #{can.canNum}</a></p>"
     end 
-  unless params[:country].nil? 
+  elsif !params[:country].nil? 
     @cans = Can.all(:country => params[:country])
     @cans.each do |can|
       output +="<a href='/display_record/#{can.id}'"
@@ -591,9 +591,8 @@ get '/displaysearch' do
     end
       output +="<img src='#{can.imageURL}' width='100' height='75 /></a>"
       output +="<p><a href='display_record/#{can.id}'>&#62 Can #{can.canNum}</a>Country = '#{can.country}'</p>"
-      exit
     end 
-  unless params[:canNum].nil?
+  elsif !params[:canNum].nil?
     @cans = Can.all(:canNum => params[:canNum])
     @cans.each do |can|
       # output +="<p>'#{@cans.count}'' records found</p>"
@@ -604,8 +603,7 @@ get '/displaysearch' do
       output += ">"
     end
       output +="<img src='#{can.imageURL}' width='100' height='75 /></a>"
-      output +="<p><a href='display_record/#{can.id}'>&#62 Can #{can.canNum}</a>Format = '#{can.canType}'</p>"
-      exit    
+      output +="<p><a href='display_record/#{can.id}'>&#62 Can #{can.canNum}</a>Format = '#{can.canType}'</p>"    
     end 
   elsif params[:canType] != "none"
     name = params[:canType].split('+')
