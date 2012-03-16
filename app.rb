b390ecfb234cd68dce4baad89153aa4346c41bd0
Matthew Epler <matthewepler@gmail.com>
2012-03-16 18:03:33 -0400

    delete country and cantype from displaysearch

--- app.rb
+++ app.rb
@@ -628,35 +628,6 @@ get '/displaysearch' do
       output +="<p><a href='display_record/#{can.id}'>&#62 Can #{can.canNum}</a>&#32 (#{can.canType})</p>"   
     end 
     
-  elsif !params[:country].nil? 
-    @cans = Can.all(:country => params[:country])
-    output +="<p>#{@cans.count} records found</p>"
-    @cans.each do |can|
-      output +="<a href='/display_record/#{can.id}'"
-      if(can.fullTrans=="yes")
-        output += " id='completed'>"
-      else
-        output += ">"
-      end
-      output +="<img src='#{can.imageURL}' width='100' height='75 /></a>"
-      output +="<p><a href='display_record/#{can.id}'>&#62 Can #{can.canNum}</a>&#32 (#{can.canType})</p>"
-    end
-   
-  elsif params[:canType] != "none"
-    name = params[:canType].split('+')
-    names = name.join(' ')
-    @cans = Can.all(:canType => names)
-    output +="<p>#{@cans.count} records found</p>"
-    @cans.each do |can|
-      output +="<a href='/display_record/#{can.id}'"
-      if(can.fullTrans=="yes")
-        output += " id='completed'>"
-      else
-       output += ">"
-      end
-      output +="<img src='#{can.imageURL}' width='100' height='75 /></a>"
-      output +="<p><a href='display_record/#{can.id}'>&#62 Can #{can.canNum}</a>&#32 (#{can.canType})</p>"
-    end  
   end
   output +="</div>"   
   output
