36141f49ad049c458aa138b185a85aa6a69f69e7
Matthew Epler <matthewepler@gmail.com>
2012-03-16 16:52:17 -0400

    changes

--- app.rb
+++ app.rb
@@ -598,7 +598,7 @@ get '/displaysearch' do
           end
         end
       end
-    end
+    
 
     # @cans = Can.all(:hasLanguage => params[:hasLanguage])
     # output +="<p>#{@cans.count} records found</p>"
