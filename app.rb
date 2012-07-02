018d22e5111ba44c7f22437edc443d3cd0cfc0de
Matthew Epler <matthewepler@gmail.com>
2012-07-02 11:53:00 -0400

    changes

--- app.rb
+++ app.rb
@@ -152,7 +152,7 @@ HTML
     name_array = Dir.entries('thumb_images')
     name_array.each do |file|
       if file.include? this.canNum.to_s && this.canType
-        link_name = file
+        link_name = file.to_s
 
       else
         link_name = name_array[0]
