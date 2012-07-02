cefa25125c6e383cd1a823b1606b778c6242ce4e
Matthew Epler <matthewepler@gmail.com>
2012-07-02 12:05:54 -0400

    clean up code blocks

--- app.rb
+++ app.rb
@@ -145,28 +145,31 @@ HTML
   output +="</div>" 
   output +='<div class="thumbs">'
   cans = Can.all(:limit => 50, :offset => offset)
+  #for each can
   for this in cans
-    link_name = nil
-    #file_name = this.canNum.to_s + " " + this.canType 
-    #find the corresponding filename
+    link_name = " "
     name_array = Dir.entries('thumb_images')
-    name_array.each do |name_string|
+
+    #go through the name array and find the matching filename
+    for name_string in name_array
       if name_string.include? this.canNum.to_s && this.canType
         link_name = name_string
+        break
       else
-        link_name = name_array[10]
+        link_name = link_name
       end
+    end
 
-      output += "<a href='/display_record/#{this.id}'"
+    output += "<a href='/display_record/#{this.id}'"
     
     if(this.fullTrans=="yes")
       output += " class='completed'>"
     else
       output += ">"
     end
+
     output += "<img src='thumb_images/#{link_name}' width='100' height='75'/></a>"
   end
-    end
     
   output +="</div>"
 
