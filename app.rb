4267103255a3951bc2043bd278ba3527cb11540b
Matthew Epler <matthewepler@gmail.com>
2012-07-02 20:47:20 -0400

    changes

--- app.rb
+++ app.rb
@@ -170,7 +170,7 @@ HTML
       output += ">"
     end
 
-    output += "<img src='http://afilmarchive.net/public/thumb_images/#{link_name}' width='100' height='75'/></a>"
+    output += "<img src='http://afilmarchive.net/thumb_images/#{link_name}' width='100' height='75'/></a>"
   end
     
   output +="</div>"
