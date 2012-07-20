0c77cebe3a1563100288bdd46ac2d7e5cf4491a7
Matthew Epler <matthewepler@gmail.com>
2012-07-20 10:58:37 -0400

    adding startpage variable to all database links

--- app.rb
+++ app.rb
@@ -129,7 +129,7 @@ HTML
   output +='<body>'
   output +='<div class="header">'
   output +='<a href="/">&#62Home</a>'
-  output +='<a href="/start/2">&#62Database</a>'
+  output +='<a href="/start/#{startpage}">&#62Database</a>'
   output +='<a href="/about">&#62About</a>'
   output +='<a href="/contact">&#62Contact</a>'
   output +='<span class="search-link"><a href="/search">&#62Search</a></span>'
@@ -323,7 +323,7 @@ HTML
 
   output +="<div class='header'>"
   output +='<a href="/">&#62Home</a>'
-  output +='<a href="/start/2">&#62Database</a>'
+  output +='<a href="/start/#{startpage}">&#62Database</a>'
   output +='<a href="/about">&#62About</a>'
   output +='<a href="/contact">&#62Contact</a>'
   output +='<span class="search-link"><a href="/search">&#62Search</a></span>'
@@ -421,7 +421,7 @@ HTML
   form +="<div class='image-side'>"
   form +="<div class='header'>"
   form +='<a href="/">&#62Home</a>'
-  form +='<a href="/start/2">&#62Database</a>'
+  form +='<a href="/start/#{startpage}">&#62Database</a>'
   form +='<a href="/about">&#62About</a>'
   form +='<a href="/contact">&#62Contact</a>'
   form +='<span class="search-link"><a href="/search">&#62Search</a></span>'
@@ -556,7 +556,7 @@ HTML
   output +="</head>"
   output +='<div class="header">'
   output +='<a href="/">&#62Home</a>'
-  output +='<a href="/start/2">&#62Database</a>'
+  output +='<a href="/start/#{startpage}">&#62Database</a>'
   output +='<a href="/about">&#62About</a>'
   output +='<a href="/contact">&#62Contact</a>'
   output +='<span class="search-link"><a href="/search">&#62Search</a></span>'
@@ -599,7 +599,7 @@ get '/about' do
 <body>
 <div class="header">
 <a href="/">&#62Home</a>
-<a href="/start/2">&#62Database</a>
+<a href="/start/#{startpage}">&#62Database</a>
 <a href="/about">&#62About</a>
 <a href="/contact">&#62Contact</a>
 <span class="search-link"><a href="/search">&#62Search</a></span>
@@ -669,7 +669,7 @@ get '/contact' do
 <body>
 <div class="header">
 <a href="/">&#62Home</a>
-<a href="/start/2">&#62Database</a>
+<a href="/start/#{startpage}">&#62Database</a>
 <a href="/about">&#62About</a>
 <a href="/contact">&#62Contact</a>
 <span class="search-link"><a href="/search">&#62Search</a></span>
@@ -710,7 +710,7 @@ HTML
   form +="<div class='image-side'>"
   form +="<div class='header'>"
   form +='<a href="/">&#62Home</a>'
-  form +='<a href="/start/2">&#62Database</a>'
+  form +='<a href="/start/#{startpage}">&#62Database</a>'
   form +='<a href="/about">&#62About</a>'
   form +='<a href="/contact">&#62Contact</a>'
   form +='<span class="search-link"><a href="/search">&#62Search</a></span>'
@@ -772,7 +772,7 @@ HTML
   output +="<div class='image-side'>"
   output +="<div class='header'>"
   output +='<a href="/">&#62Home</a>'
-  output +='<a href="/start/2">&#62Database</a>'
+  output +='<a href="/start/#{startpage}">&#62Database</a>'
   output +='<a href="/about">&#62About</a>'
   output +='<a href="/contact">&#62Contact</a>'
   output +='<span class="search-link"><a href="/search">&#62Search</a></span>'
